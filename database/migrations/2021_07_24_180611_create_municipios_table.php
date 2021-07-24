8906c27fb6507c688021f41760caaeb981cbda30
Clayton Martins Laia <48263294+claytonmlaia@users.noreply.github.com>
2021-07-24 15:50:30 -0300

    Migrations
    Criação das migrations do projeto, ja com as devidas ligações.

---
+++ database/migrations/2021_07_24_180611_create_municipios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ufs_id');
+            $table->string('municipio');
+            $table->timestamps();
+            //ligacao
+            $table->foreign('ufs_id')->references('id')->on('ufs')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('municipios');
+    }
+}
---
+++ database/migrations/2021_07_24_180834_create_usuarios_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ufs_id');
+            $table->unsignedBigInteger('escolaridades_id');
+            $table->unsignedBigInteger('municipios_id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->date('data_nascimento');
+            $table->timestamps();
+            // ligacoes
+            $table->foreign('ufs_id')->references('id')->on('ufs')->onDelete('CASCADE');
+            $table->foreign('escolaridades_id')->references('id')->on('escolaridades')->onDelete('CASCADE');
+            $table->foreign('municipios_id')->references('id')->on('municipios')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
